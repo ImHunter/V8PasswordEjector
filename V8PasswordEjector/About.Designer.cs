24adccadd3ab80f9c2da3780354fc993ee2a862e
YPermitin <ypermitin@yandex.ru>
2017-01-13 21:02:16 +0500

    Улучшена форма "О приложении"
    Удалены неиспользуемые ссылки
    Даны логически верные наименования элементам формы
    Добавлена ссылка на GitHub

--- V8PasswordEjector/About.Designer.cs
+++ V8PasswordEjector/About.Designer.cs
@@ -28,172 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.linkLabelEmail1 = new System.Windows.Forms.LinkLabel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.labelName1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.linkLabelBlog = new System.Windows.Forms.LinkLabel();
+            this.linkLblAuthorEmail = new System.Windows.Forms.LinkLabel();
+            this.lblAuthorHeader = new System.Windows.Forms.Label();
+            this.lblAuthorName = new System.Windows.Forms.Label();
+            this.lblEmailHeader = new System.Windows.Forms.Label();
+            this.lblBlogHeader = new System.Windows.Forms.Label();
+            this.linkLblAuthorWebsite = new System.Windows.Forms.LinkLabel();
             this.richTextBoxInfo = new System.Windows.Forms.RichTextBox();
-            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.labelName2 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.linkLabelEmail2 = new System.Windows.Forms.LinkLabel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lblCoAuthorHeader = new System.Windows.Forms.Label();
+            this.lblCoAuthorName = new System.Windows.Forms.Label();
+            this.lblEmailCoAuthorHeader = new System.Windows.Forms.Label();
+            this.linkLblCoAuthorEmail = new System.Windows.Forms.LinkLabel();
+            this.lblVer = new System.Windows.Forms.Label();
+            this.lblVerHeader = new System.Windows.Forms.Label();
             this.btnSendFeedback = new System.Windows.Forms.Button();
+            this.linkLblGitHub = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // linkLabelEmail1
+            // linkLblAuthorEmail
             // 
-            this.linkLabelEmail1.AutoSize = true;
-            this.linkLabelEmail1.Location = new System.Drawing.Point(343, 26);
-            this.linkLabelEmail1.Name = "linkLabelEmail1";
-            this.linkLabelEmail1.Size = new System.Drawing.Size(105, 13);
-            this.linkLabelEmail1.TabIndex = 0;
-            this.linkLabelEmail1.TabStop = true;
-            this.linkLabelEmail1.Text = "ypermitin@yandex.ru";
-            this.linkLabelEmail1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.linkLblAuthorEmail.AutoSize = true;
+            this.linkLblAuthorEmail.Location = new System.Drawing.Point(100, 26);
+            this.linkLblAuthorEmail.Name = "linkLblAuthorEmail";
+            this.linkLblAuthorEmail.Size = new System.Drawing.Size(105, 13);
+            this.linkLblAuthorEmail.TabIndex = 0;
+            this.linkLblAuthorEmail.TabStop = true;
+            this.linkLblAuthorEmail.Text = "ypermitin@yandex.ru";
+            this.linkLblAuthorEmail.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
-            // label1
+            // lblAuthorHeader
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Автор:";
+            this.lblAuthorHeader.AutoSize = true;
+            this.lblAuthorHeader.Location = new System.Drawing.Point(12, 9);
+            this.lblAuthorHeader.Name = "lblAuthorHeader";
+            this.lblAuthorHeader.Size = new System.Drawing.Size(40, 13);
+            this.lblAuthorHeader.TabIndex = 1;
+            this.lblAuthorHeader.Text = "Автор:";
             // 
-            // labelName1
+            // lblAuthorName
             // 
-            this.labelName1.AutoSize = true;
-            this.labelName1.Location = new System.Drawing.Point(365, 13);
-            this.labelName1.Name = "labelName1";
-            this.labelName1.Size = new System.Drawing.Size(83, 13);
-            this.labelName1.TabIndex = 2;
-            this.labelName1.Text = "Пермитин Ю.А.";
+            this.lblAuthorName.AutoSize = true;
+            this.lblAuthorName.Location = new System.Drawing.Point(122, 13);
+            this.lblAuthorName.Name = "lblAuthorName";
+            this.lblAuthorName.Size = new System.Drawing.Size(83, 13);
+            this.lblAuthorName.TabIndex = 2;
+            this.lblAuthorName.Text = "Пермитин Ю.А.";
             // 
-            // label3
+            // lblEmailHeader
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 26);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "E-mail:";
+            this.lblEmailHeader.AutoSize = true;
+            this.lblEmailHeader.Location = new System.Drawing.Point(12, 26);
+            this.lblEmailHeader.Name = "lblEmailHeader";
+            this.lblEmailHeader.Size = new System.Drawing.Size(38, 13);
+            this.lblEmailHeader.TabIndex = 3;
+            this.lblEmailHeader.Text = "E-mail:";
             // 
-            // label4
+            // lblBlogHeader
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 44);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(34, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Блог:";
+            this.lblBlogHeader.AutoSize = true;
+            this.lblBlogHeader.Location = new System.Drawing.Point(12, 44);
+            this.lblBlogHeader.Name = "lblBlogHeader";
+            this.lblBlogHeader.Size = new System.Drawing.Size(34, 13);
+            this.lblBlogHeader.TabIndex = 4;
+            this.lblBlogHeader.Text = "Блог:";
             // 
-            // linkLabelBlog
+            // linkLblAuthorWebsite
             // 
-            this.linkLabelBlog.AutoSize = true;
-            this.linkLabelBlog.Location = new System.Drawing.Point(364, 44);
-            this.linkLabelBlog.Name = "linkLabelBlog";
-            this.linkLabelBlog.Size = new System.Drawing.Size(82, 13);
-            this.linkLabelBlog.TabIndex = 5;
-            this.linkLabelBlog.TabStop = true;
-            this.linkLabelBlog.Text = "develplatform.ru";
-            this.linkLabelBlog.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            this.linkLblAuthorWebsite.AutoSize = true;
+            this.linkLblAuthorWebsite.Location = new System.Drawing.Point(121, 44);
+            this.linkLblAuthorWebsite.Name = "linkLblAuthorWebsite";
+            this.linkLblAuthorWebsite.Size = new System.Drawing.Size(82, 13);
+            this.linkLblAuthorWebsite.TabIndex = 5;
+            this.linkLblAuthorWebsite.TabStop = true;
+            this.linkLblAuthorWebsite.Text = "develplatform.ru";
+            this.linkLblAuthorWebsite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
             // 
             // richTextBoxInfo
             // 
-            this.richTextBoxInfo.Location = new System.Drawing.Point(13, 161);
+            this.richTextBoxInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxInfo.Cursor = System.Windows.Forms.Cursors.No;
+            this.richTextBoxInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxInfo.Location = new System.Drawing.Point(15, 130);
             this.richTextBoxInfo.Name = "richTextBoxInfo";
             this.richTextBoxInfo.ReadOnly = true;
-            this.richTextBoxInfo.Size = new System.Drawing.Size(433, 102);
+            this.richTextBoxInfo.Size = new System.Drawing.Size(436, 70);
             this.richTextBoxInfo.TabIndex = 7;
             this.richTextBoxInfo.Text = "Программа для сброса учетных записей в информационной базе 1С:Предприятия 8.x.  \n" +
     "\nАвтор не несет ответственности за использование приложения в корыстных целях.";
             // 
-            // linkLabel3
+            // lblCoAuthorHeader
             // 
-            this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(276, 266);
-            this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(170, 13);
-            this.linkLabel3.TabIndex = 8;
-            this.linkLabel3.TabStop = true;
-            this.linkLabel3.Text = "Статья \"Вход без приглашения\"";
-            this.linkLabel3.Click += new System.EventHandler(this.linkLabel3_Click);
+            this.lblCoAuthorHeader.AutoSize = true;
+            this.lblCoAuthorHeader.Location = new System.Drawing.Point(264, 13);
+            this.lblCoAuthorHeader.Name = "lblCoAuthorHeader";
+            this.lblCoAuthorHeader.Size = new System.Drawing.Size(52, 13);
+            this.lblCoAuthorHeader.TabIndex = 10;
+            this.lblCoAuthorHeader.Text = "Соавтор:";
             // 
-            // linkLabel4
+            // lblCoAuthorName
             // 
-            this.linkLabel4.AutoSize = true;
-            this.linkLabel4.Location = new System.Drawing.Point(10, 266);
-            this.linkLabel4.Name = "linkLabel4";
-            this.linkLabel4.Size = new System.Drawing.Size(233, 13);
-            this.linkLabel4.TabIndex = 9;
-            this.linkLabel4.TabStop = true;
-            this.linkLabel4.Text = "Статья \"Сброс учетных записей. Пишем у...\"";
-            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            this.lblCoAuthorName.AutoSize = true;
+            this.lblCoAuthorName.Location = new System.Drawing.Point(365, 13);
+            this.lblCoAuthorName.Name = "lblCoAuthorName";
+            this.lblCoAuthorName.Size = new System.Drawing.Size(80, 13);
+            this.lblCoAuthorName.TabIndex = 11;
+            this.lblCoAuthorName.Text = "Плеханов П.С.";
             // 
-            // label5
+            // lblEmailCoAuthorHeader
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 72);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Соавтор:";
+            this.lblEmailCoAuthorHeader.AutoSize = true;
+            this.lblEmailCoAuthorHeader.Location = new System.Drawing.Point(264, 26);
+            this.lblEmailCoAuthorHeader.Name = "lblEmailCoAuthorHeader";
+            this.lblEmailCoAuthorHeader.Size = new System.Drawing.Size(38, 13);
+            this.lblEmailCoAuthorHeader.TabIndex = 12;
+            this.lblEmailCoAuthorHeader.Text = "E-mail:";
             // 
-            // labelName2
+            // linkLblCoAuthorEmail
             // 
-            this.labelName2.AutoSize = true;
-            this.labelName2.Location = new System.Drawing.Point(368, 72);
-            this.labelName2.Name = "labelName2";
-            this.labelName2.Size = new System.Drawing.Size(80, 13);
-            this.labelName2.TabIndex = 11;
-            this.labelName2.Text = "Плеханов П.С.";
+            this.linkLblCoAuthorEmail.AutoSize = true;
+            this.linkLblCoAuthorEmail.Location = new System.Drawing.Point(340, 26);
+            this.linkLblCoAuthorEmail.Name = "linkLblCoAuthorEmail";
+            this.linkLblCoAuthorEmail.Size = new System.Drawing.Size(103, 13);
+            this.linkLblCoAuthorEmail.TabIndex = 13;
+            this.linkLblCoAuthorEmail.TabStop = true;
+            this.linkLblCoAuthorEmail.Text = "psplehanov@mail.ru";
             // 
-            // label7
+            // lblVer
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 85);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(38, 13);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "E-mail:";
+            this.lblVer.AutoSize = true;
+            this.lblVer.Location = new System.Drawing.Point(163, 75);
+            this.lblVer.Name = "lblVer";
+            this.lblVer.Size = new System.Drawing.Size(40, 13);
+            this.lblVer.TabIndex = 15;
+            this.lblVer.Text = "2.0.0.0";
             // 
-            // linkLabelEmail2
+            // lblVerHeader
             // 
-            this.linkLabelEmail2.AutoSize = true;
-            this.linkLabelEmail2.Location = new System.Drawing.Point(343, 85);
-            this.linkLabelEmail2.Name = "linkLabelEmail2";
-            this.linkLabelEmail2.Size = new System.Drawing.Size(103, 13);
-            this.linkLabelEmail2.TabIndex = 13;
-            this.linkLabelEmail2.TabStop = true;
-            this.linkLabelEmail2.Text = "psplehanov@mail.ru";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(406, 116);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 15;
-            this.label2.Text = "2.0.0.0";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 116);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 13);
-            this.label6.TabIndex = 14;
-            this.label6.Text = "Версия:";
+            this.lblVerHeader.AutoSize = true;
+            this.lblVerHeader.Location = new System.Drawing.Point(12, 75);
+            this.lblVerHeader.Name = "lblVerHeader";
+            this.lblVerHeader.Size = new System.Drawing.Size(47, 13);
+            this.lblVerHeader.TabIndex = 14;
+            this.lblVerHeader.Text = "Версия:";
             // 
             // btnSendFeedback
             // 
-            this.btnSendFeedback.Location = new System.Drawing.Point(15, 132);
+            this.btnSendFeedback.Location = new System.Drawing.Point(15, 101);
             this.btnSendFeedback.Name = "btnSendFeedback";
             this.btnSendFeedback.Size = new System.Drawing.Size(433, 23);
             this.btnSendFeedback.TabIndex = 16;
@@ -201,27 +181,38 @@
             this.btnSendFeedback.UseVisualStyleBackColor = true;
             this.btnSendFeedback.Click += new System.EventHandler(this.btnSendFeedback_Click);
             // 
+            // linkLblGitHub
+            // 
+            this.linkLblGitHub.AutoSize = true;
+            this.linkLblGitHub.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLblGitHub.Location = new System.Drawing.Point(264, 72);
+            this.linkLblGitHub.Name = "linkLblGitHub";
+            this.linkLblGitHub.Size = new System.Drawing.Size(55, 16);
+            this.linkLblGitHub.TabIndex = 17;
+            this.linkLblGitHub.TabStop = true;
+            this.linkLblGitHub.Text = "GitHub\r\n";
+            this.linkLblGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLblGitHub_LinkClicked);
+            // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(460, 287);
+            this.ClientSize = new System.Drawing.Size(457, 206);
+            this.Controls.Add(this.linkLblGitHub);
             this.Controls.Add(this.btnSendFeedback);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.linkLabelEmail2);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.labelName2);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.linkLabel4);
-            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.lblVer);
+            this.Controls.Add(this.lblVerHeader);
+            this.Controls.Add(this.linkLblCoAuthorEmail);
+            this.Controls.Add(this.lblEmailCoAuthorHeader);
+            this.Controls.Add(this.lblCoAuthorName);
+            this.Controls.Add(this.lblCoAuthorHeader);
             this.Controls.Add(this.richTextBoxInfo);
-            this.Controls.Add(this.linkLabelBlog);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.labelName1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.linkLabelEmail1);
+            this.Controls.Add(this.linkLblAuthorWebsite);
+            this.Controls.Add(this.lblBlogHeader);
+            this.Controls.Add(this.lblEmailHeader);
+            this.Controls.Add(this.lblAuthorName);
+            this.Controls.Add(this.lblAuthorHeader);
+            this.Controls.Add(this.linkLblAuthorEmail);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -236,21 +227,20 @@
 
         #endregion
 
-        private System.Windows.Forms.LinkLabel linkLabelEmail1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label labelName1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.LinkLabel linkLabelBlog;
+        private System.Windows.Forms.LinkLabel linkLblAuthorEmail;
+        private System.Windows.Forms.Label lblAuthorHeader;
+        private System.Windows.Forms.Label lblAuthorName;
+        private System.Windows.Forms.Label lblEmailHeader;
+        private System.Windows.Forms.Label lblBlogHeader;
+        private System.Windows.Forms.LinkLabel linkLblAuthorWebsite;
         private System.Windows.Forms.RichTextBox richTextBoxInfo;
-        private System.Windows.Forms.LinkLabel linkLabel3;
-        private System.Windows.Forms.LinkLabel linkLabel4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label labelName2;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.LinkLabel linkLabelEmail2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblCoAuthorHeader;
+        private System.Windows.Forms.Label lblCoAuthorName;
+        private System.Windows.Forms.Label lblEmailCoAuthorHeader;
+        private System.Windows.Forms.LinkLabel linkLblCoAuthorEmail;
+        private System.Windows.Forms.Label lblVer;
+        private System.Windows.Forms.Label lblVerHeader;
         private System.Windows.Forms.Button btnSendFeedback;
+        private System.Windows.Forms.LinkLabel linkLblGitHub;
     }
 }
--- V8PasswordEjector/About.cs
+++ V8PasswordEjector/About.cs
@@ -11,11 +11,6 @@ namespace DevelPlatform.OneCEUtils.AccessGranted
             InitializeComponent();
         }
 
-        private void linkLabel3_Click(object sender, EventArgs e)
-        {
-            Process.Start("http://develplatform.ru/OneC/Stream/OneCBlog/2012/12/07/Вход-без-приглашения");
-        }
-
         private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Process.Start("http://develplatform.ru");
@@ -28,12 +23,17 @@ namespace DevelPlatform.OneCEUtils.AccessGranted
 
         private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Process.Start("http://develplatform.ru/OneC/Stream/OneCBlog/2013/05/11/Сброс-учетных-записей-Пишем-универсальную-программу-на-NET-Framework");
+            
         }
 
         private void btnSendFeedback_Click(object sender, EventArgs e)
         {
             Process.Start("http://www.develplatform.ru/About-us/Contact-us?messageSubject=%D0%A1%D0%B1%D1%80%D0%BE%D1%81%20%D1%83%D1%87%D0%B5%D1%82%D0%BD%D1%8B%D1%85%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B5%D0%B9%201C:%D0%9F%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%B8%D1%8F%D1%82%D0%B8%D1%8F%20(%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5)");
+        }
+
+        private void linkLblGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/YPermitin/V8PasswordEjector");
         }
     }
 }
